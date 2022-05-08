dceeb883ba0320410aca20553622fa4b285b4e4c
pimpbot9000 <t.valimaki@gmail.com>
2022-05-08 20:56:01 +0300

    Change db-connection

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -3,6 +3,15 @@ const knex = require('knex')
 const knexfile = require('./knexfile')[process.env.NODE_ENV]
 
 console.log("Connect to database")
-const db = knex(knexfile)
+
+
+const createUnixSocketPool = async () => {
+    // Note: Saving credentials in environment variables is convenient, but not
+    // secure - consider a more secure solution such as
+    // Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+    // keep secrets safe.
+    return await Knex(knexfile);
+  }
+db = createUnixSocketPool()
 
 module.exports = db
